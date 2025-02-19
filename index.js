5e0dc93b0deaf0036da7f78a0bd074dba3773c50
Kamsi Nnaegbuna <kamsinnaegbuna@gmail.com>
2025-02-19 11:07:31 +0100

    Add health check endpoint and improve UI with preloader and header

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+app.get('/health', (req, res) => {
+    res.status(200).json({
+        status: 'ok'
+    });
+})
+
 app.listen(PORT, err => {
     console.log(`listening on ${PORT}`);
 })
